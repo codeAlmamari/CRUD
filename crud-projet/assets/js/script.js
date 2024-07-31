d6fa0db4722817d2a76d6d45a334d39e66ad398b
Abdulrahman Almamari <codemamari@gmail.com>
2024-08-01 00:41:39 +0400

    Add files via upload

---
+++ crud-projet/assets/js/script.js
@@ -0,0 +1,26 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let categury = document.getElementById('categury');
+let submit = document.getElementById('submit');
+
+
+// get totlal
+function getTotal()
+{
+    console.log("done")
+}
+
+// craet Product
+// save to local storage
+// clear inputs
+// raed
+// count
+// delete
+// update
+// search
+// clean data
